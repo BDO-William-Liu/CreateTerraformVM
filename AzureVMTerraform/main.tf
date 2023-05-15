e5174005bcfec848e40b1fd08d27a6ae82584e3f
Liu <wiliu@bdo.ca>
2023-05-15 10:15:14 -0400

    fixed value of azureurm_network_interface resource

--- AzureVMTerraform/main.tf
+++ AzureVMTerraform/main.tf
@@ -13,8 +13,8 @@ resource "azurerm_subnet" "mySubnet" {
 
 resource "azurerm_network_interface" "myNetworkInterface" {
   name                = "${var.name}-NIC"
-  location            = azurerm_resource_group.myResourceGroup
-  resource_group_name = azurerm_resource_group.myResourceGroup
+  location            = azurerm_resource_group.myResourceGroup.location
+  resource_group_name = azurerm_resource_group.myResourceGroup.name
 
   ip_configuration {
     name                          = "internal"
