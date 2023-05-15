34635378541ea1e2af433e93e81c9f4a96ff3fd3
Liu <wiliu@bdo.ca>
2023-05-15 15:25:05 -0400

    updated os_disk and os_profile

--- AzureVMTerraform/main.tf
+++ AzureVMTerraform/main.tf
@@ -4,7 +4,6 @@ resource "azurerm_subnet" "mySubnet" {
   virtual_network_name = var.virtual_network
   address_prefixes     = var.address_prefix
 }
-// mySubnet depends on name argument of myResourceGroup
 
 resource "azurerm_network_interface" "myNetworkInterface" {
   name                = "${var.name}-NIC"
@@ -17,29 +16,37 @@ resource "azurerm_network_interface" "myNetworkInterface" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-// myNetworkInterface depends on name argument of myResourceGroup and id attribute reference of mySubnet
 
 resource "azurerm_windows_virtual_machine" "myVirtualMachine" {
   name                = var.name
   resource_group_name = azurerm_subnet.mySubnet.resource_group_name
   location            = azurerm_network_interface.myNetworkInterface.location
   size                = var.size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
   network_interface_ids = [
     azurerm_network_interface.myNetworkInterface.id
   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  storage_os_disk {
+    name              = "myOSDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
+
+  os_profile {
+    computer_name  = var.name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
-// myVirtualMachine depends on arguments of myResourceGroup, mySubnet, and myVirtualInterface
